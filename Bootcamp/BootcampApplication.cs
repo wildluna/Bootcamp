d9457aceaa6c04ed1f2f2ad1be8eabfc84ed28a1
Administrator <Administrator@TONY-PC>
2017-08-01 16:18:32 +0800

    加入專案檔案。

---
+++ Bootcamp/BootcampApplication.cs
@@ -0,0 +1,22 @@
+﻿using Photon.SocketServer;
+
+namespace Bootcamp
+{
+    public class BootcampApplication : Photon.SocketServer.ApplicationBase
+    {
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            return new BootcampPeer(initRequest);
+        }
+
+        protected override void Setup()
+        {
+            
+        }
+
+        protected override void TearDown()
+        {
+            
+        }
+    }
+}
---
+++ Bootcamp/BootcampPeer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+
+namespace Bootcamp
+{
+    class BootcampPeer : ClientPeer
+    {
+        public BootcampPeer(InitRequest initRequest)
+            : base(initRequest)
+        {
+        }
+
+        protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+        {
+        }
+
+        protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+            SendOperationResponse(new OperationResponse(operationRequest.OperationCode, operationRequest.Parameters), sendParameters);
+        }
+    }
+}
